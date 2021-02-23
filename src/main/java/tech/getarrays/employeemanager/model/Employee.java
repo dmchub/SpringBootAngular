cce5850297179100f2d5a5686d6b61ff8079f12a
dmchub <37676729+dmchub@users.noreply.github.com>
2021-02-24 01:07:28 +0300

    Initial commit

---
+++ src/main/java/tech/getarrays/employeemanager/model/Employee.java
@@ -0,0 +1,21 @@
+package tech.getarrays.employeemanager.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+    
+}
